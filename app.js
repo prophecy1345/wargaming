4fac8614f91b4152693d31c0af381d198aaa1a80
Booker Dewitt <booker@swapspace.co>
2024-04-13 19:56:16 +0200

    Refactor app.js and index.html to add dynamic tank functionality

--- app.js
+++ app.js
@@ -5,3 +5,28 @@ $(document).ready(function() {
         $('.tanks-container').append(clone);
     }
 });
+
+$("#rating_slider").slider({
+  min: 0,
+  max: 300,
+  value: 1,
+  slide: function (event, ui) {
+    //When sliding the toggle
+    updateInputFromSlider("#rating_input", $(this).slider("value"));
+  },
+  change: function (event, ui) {
+    //After releasing the toggle
+    updateInputFromSlider("#rating_input", $(this).slider("value"));
+  }
+});
+
+function updateInputFromSlider(input_id, value) {
+  $(input_id).val(value);
+}
+
+$(document).ready(function () {
+  $("#rating_input").change(function () {
+    $("#rating_slider").slider("value", $(this).val());
+    $("#rating_slider").prop("value", $(this).val());
+  });
+});
